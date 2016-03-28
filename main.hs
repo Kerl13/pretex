3821825f9ae17c5f70a88a7f0a7267029baedf09
Kerl13 <martin.pepin@netcourrier.com>
2016-03-28 16:35:33 +0200

    main is better like that

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ import Lexer (get_tokens)
 import Parser (parse)
 import Compile (compile)
 
-main = do
-	c <- getContents
-	putStr . compile . parse . get_tokens $ c
+run :: String -> String
+run = compile . parse . get_tokens
+
+main = interact run
